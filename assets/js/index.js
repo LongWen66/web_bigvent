89823bc11ef06f9bb161bb59c197b5e6ccd14f62
long <1072362251@qq.com>
2022-03-17 15:25:47 +0800

    完成了主页开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -30,16 +30,16 @@ function getUserInfo() {
             }
             renderAvatar(res.data)
         },
-        // 不论成功还是失败，最终都会调用 complete 回调函数
-        complete: function (res) {
-            console.log(res);
-            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-                // 1. 强制清空 token
-                localStorage.removeItem('token')
-                // 2. 强制跳转到登录页面
-                location.href = '/login.html'
-            }
-        }
+        // // 不论成功还是失败，最终都会调用 complete 回调函数
+        // complete: function (res) {
+        //     console.log(res);
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //         // 1. 强制清空 token
+        //         localStorage.removeItem('token')
+        //         // 2. 强制跳转到登录页面
+        //         location.href = '/login.html'
+        //     }
+        // }
 
     })
 
